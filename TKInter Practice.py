3b125657ade3ef1ba225224ee654c719f9d4108d
huh445 <127354663+huh445@users.noreply.github.com>
2024-03-25 21:14:06 +1100

    YEP

--- TKInter Practice.py
+++ TKInter Practice.py
@@ -4,12 +4,16 @@ root = tk.Tk()
 
 currentScore1 = 0
 
-root.title("Scoreboard")
+root.title("...and be counted app")
 
 def increaseScore1():
     global currentScore1
     currentScore1 = currentScore1 + 1
     Score1.configure(text = currentScore1)
+    if currentScore1 > 200:
+        capacityLabel.config(text="At capacity")
+    else:
+        capacityLabel.config(text="Not at capacity")
 
 def decreaseScore1():
     global currentScore1
@@ -39,6 +43,21 @@ decreaseScoreButton.pack()
 resetScoreButton = tk.Button(root, text="RESET COUNTER", command=resetScore)
 resetScoreButton.pack()
 
+NWAlignedLabel = tk.Label(root, text="...and be counted app")
+NWAlignedLabel.pack(anchor="nw")
+
+ExitButton = tk.Button(root, text="Exit", command=root.destroy)
+ExitButton.pack(anchor="se", side="bottom")
+
+headingLabel = tk.Label(root, text="...and be counted app")
+headingLabel.pack(anchor="center")
+
+subHeadingLabel = tk.Label(root, text="App that counts the number of poeple entering the stand")
+subHeadingLabel.pack(anchor="center")
+
+capacityLabel = tk.Label(root, text="Not at capacity")
+capacityLabel.pack(anchor="center")
+
 increaseScoreButton.configure(width=5, height=2)
 increaseScoreButton.config(bg="#000000",fg="#ffffff",font=("Arial",50))
 
@@ -47,10 +66,24 @@ decreaseScoreButton.config(bg="#000000",fg="#ffffff",font=("Arial",50))
 
 resetScoreButton.config(bg="#000000",fg="#ffffff",font=("Arial",18))
 
+Score1.configure(width=7, height=2)
 Score1.config(fg="#000000",font=("Arial",50), borderwidth=1, relief="solid")
 
-increaseScoreButton.place(x=300, y=100)
-decreaseScoreButton.place(x=750, y=100)
+ExitButton.config(bg="#000000", fg="#ffffff", font=("Arial"))
+
+NWAlignedLabel.config(fg="#000000",font=("Arial",10))
+
+headingLabel.config(font=("Arial",18))
+
+subHeadingLabel.config(font=("Arial", 10))
+
+capacityLabel.config(font=("Arial", 18), anchor="center")
+
+increaseScoreButton.place(x=850, y=200)
+decreaseScoreButton.place(x=200, y=200)
+Score1.place(x=500, y=200)
+resetScoreButton.place(x=525, y=425)
+capacityLabel.place(x=550,y=150)
 
 root.geometry("1280x720")
 root.mainloop()
