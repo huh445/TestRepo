d1c16d66d98430ac7ac672a36d12baa4ac29bbd6
huh445 <127354663+huh445@users.noreply.github.com>
2024-03-25 13:06:59 +1100

    Update import tkinter as tk.py

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -3,7 +3,6 @@ import tkinter as tk
 root = tk.Tk()
 
 currentScore1 = 0
-currentScore2 = 0
 
 root.title("Scoreboard")
 
@@ -11,6 +10,7 @@ def increaseScore1():
     global currentScore1
     currentScore1 = currentScore1 + 1
     Score1.configure(text = currentScore1)
+
 def decreaseScore1():
     global currentScore1
     currentScore1 = currentScore1 - 1
@@ -20,6 +20,11 @@ def decreaseScore1():
         Score1.configure(text = currentScore1)
     else:
         Score1.configure(text = currentScore1)
+
+def resetScore():
+    global currentScore1
+    currentScore1 = 0
+    Score1.configure(text=currentScore1)
 
 
 
@@ -32,31 +37,8 @@ increaseScore1Button.pack()
 decreaseScore1Button = tk.Button(root, text="Decrease Score 1 By 1", command=decreaseScore1)
 decreaseScore1Button.pack()
 
-def increaseScore2():
-    global currentScore2
-    currentScore2 =+ 1
-    Score2.configure(text = currentScore2)
-def decreaseScore2():
-    global currentScore2
-    currentScore2 = currentScore2 - 1
-    if currentScore2 < 0:
-        print("The score can only be positive.")
-        currentScore2 = 0
-        Score2.configure(text = currentScore2)
-    else:
-        Score2.configure(text = currentScore2)
-
-
-
-Score2 = tk.Label(root)
-Score2.pack()
-
-increaseScore2Button = tk.Button(root, text="Increase Score 2 By 1", command=increaseScore2)
-increaseScore2Button.pack()
-
-decreaseScore2Button = tk.Button(root, text="Decrease Score 2 By 1", command=decreaseScore2)
-decreaseScore2Button.pack()
-
+resetScore1 = tk.Button(root, text="Reset", command=resetScore)
+resetScore1.pack()
 
 root.geometry("640x480")
 root.mainloop()
