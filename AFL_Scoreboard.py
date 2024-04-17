6d61e60f1f62b60ffe5141dd8237c2b9f2fca508
huh445 <127354663+huh445@users.noreply.github.com>
2024-04-17 11:05:50 +1000

    done

--- AFL_Scoreboard.py
+++ AFL_Scoreboard.py
@@ -5,9 +5,14 @@ root = tk.Tk()
 root.title("AFL_Scoreboard")
 
 currentScore1 = 0
-currentscore2 = 0
+currentScore2 = 0
 
 def increaseScore1():
+    global currentScore1
+    currentScore1 = currentScore1 + 6
+    Score1.configure(text = currentScore1)
+    
+def behindScore1():
     global currentScore1
     currentScore1 = currentScore1 + 1
     Score1.configure(text = currentScore1)
@@ -31,9 +36,13 @@ def resetScore():
 
 def increaseScore2():
     global currentScore2
-    currentScore2 = currentScore2 + 1
-    Score1.configure(text = currentScore2)
+    currentScore2 = currentScore2 + 6
+    Score2.configure(text = currentScore2)
 
+def behindScore2():
+    global currentScore2
+    currentScore2 = currentScore2 + 1
+    Score2.configure(text = currentScore2)
 
 def decreaseScore2():
     global currentScore2
@@ -45,25 +54,34 @@ def decreaseScore2():
     else:
         Score2.configure(text = currentScore2)
 
-increaseScoreButton1 = tk.Button(root, text="Increase The Johns by One", command=increaseScore1)
-increaseScoreButton1.pack()
+goalScoreButton1 = tk.Button(root, text="GOAL JOHNS", command=increaseScore1)
+goalScoreButton1.grid(row=1, column=1, pady=20, padx=20)
 
-decreaseScoreButton1 = tk.Button(root, text="Decrease The Johns by One", command=decreaseScore1)
-decreaseScoreButton1.pack()
+goalScoreButton1 = tk.Button(root, text="BEHIND JOHNS", command=behindScore1)
+goalScoreButton1.grid(row=2, column=1, pady=20, padx=20)
 
-increaseScoreButton2 = tk.Button(root, text="Increase The Ethans by One", command=increaseScore2)
-increaseScoreButton2.pack()
+decreaseScoreButton1 = tk.Button(root, text="DECREASE JOHNS", command=decreaseScore1)
+decreaseScoreButton1.grid(row=3, column=1, pady=20, padx=20)
 
-decreaseScoreButton2 = tk.Button(root, text="Decrease The Ethans by One", command=decreaseScore2)
-decreaseScoreButton2.pack()
+increaseScoreButton2 = tk.Button(root, text="GOAL ETHANS", command=increaseScore2)
+increaseScoreButton2.grid(row=1, column=10, pady=20, padx=20)
+
+goalScoreButton2 = tk.Button(root, text="GOAL JOHNS", command=behindScore2)
+goalScoreButton2.grid(row=2, column=10, pady=20, padx=20)
+
+decreaseScoreButton2 = tk.Button(root, text="DECREASE ETHANS", command=decreaseScore2)
+decreaseScoreButton2.grid(row=3, column=10, pady=20, padx=20)
 
 Score1 = tk.Label(root)
-Score1.pack()
+Score1.grid(row=0, column=1, pady=20, padx=20)
+
 
 Score2 = tk.Label(root)
-Score2.pack()
+Score2.grid(row=0, column=10, pady=20, padx=20)
 
+exitButton = tk.Button(root, text="Exit", command=root.destroy)
+exitButton.grid(sticky="se", column=10)
 
-root.geometry("800x800")
+root.geometry("300x300")
 
 root.mainloop()
