5a4eeae4bd47cfaf3d1e6785cf7233f4a017a371
huh445 <127354663+huh445@users.noreply.github.com>
2024-05-22 20:08:20 +1000

    yarp

--- john/main.py
+++ john/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from scoring import Scoring
 
 class ScoringApp:
     def __init__(self, root, scoring):
@@ -44,72 +45,9 @@ class ScoringApp:
         self.p2ScoreDecrease.pack(pady=5)
         self.resetButton.pack(pady=20)
 
-
-class Scoring:
-    def __init__(self):
-        self.p1Points, self.p1Games, self.p1Sets = 0, 0, 0
-        self.p2Points, self.p2Games, self.p2Sets = 0, 0, 0
-
-    def reset(self):
-        self.p1Points, self.p1Games, self.p1Sets = 0, 0, 0
-        self.p2Points, self.p2Games, self.p2Sets = 0, 0, 0
-        app.update()
-
-    def p1Scores(self):
-        self.p1Points += 1
-        if self.p1Points >= 11 and (self.p1Points - self.p2Points) >= 2:
-            self.p1Games += 1
-            self.p1Save, self.p2Save = self.p1Points, self.p2Points
-            self.p1Points, self.p2Points = 0, 0
-            
-        if self.p1Games >= 3:
-            self.p1Sets += 1
-            self.p1Points, self.p2Points, self.p1Games, self.p2Games = 0, 0, 0, 0
-        app.update()
-
-    def p1Decrease(self):
-        self.p1Points -= 1
-        if self.p1Points < 0:
-            self.p1Points = 0
-            if self.p1Sets >= 1:
-                self.p1Sets -= 1
-                self.p1Games = 2
-                self.p1Points = self.p1Save - 1
-                self.p2Points = self.p2Save
-            elif self.p1Games >= 1:
-                self.p1Games -= 1
-                self.p1Points = self.p1Save - 1
-                self.p2Points = self.p2Save
-        app.update()
-
-    def p2Scores(self):
-        self.p2Points += 1
-        if self.p2Points >= 11 and (self.p2Points - self.p1Points) >= 2:
-            self.p2Games += 1
-            self.p1Points, self.p2Points = 0, 0
-            self.p1Save, self.p2Save = self.p1Points, self.p2Points
-        if self.p2Games >= 3:
-            self.p2Sets += 1
-            self.p1Points, self.p2Points, self.p1Games, self.p2Games = 0, 0, 0, 0
-        app.update()
-
-    def p2Decrease(self):
-        self.p2Points -= 1
-        if self.p2Points < 0:
-            self.p2Points = 0
-            if self.p2Sets >= 1:
-                self.p2Sets -= 1
-                self.p2Games = 2
-                self.p2Points = self.p2Save - 1
-                self.p1Points = self.p1Save
-            elif self.p2Games >= 1:
-                self.p2Games -= 1
-                self.p2Points = self.p2Save - 1
-                self.p1Points = self.p1Save
-        app.update()
-
 if __name__ == "__main__":
     root = tk.Tk()
-    scoring = Scoring()
+    scoring = Scoring(None)  # Pass None initially
     app = ScoringApp(root, scoring)
+    scoring.app = app  # Assign the ScoringApp instance to the Scoring class
     root.mainloop()
