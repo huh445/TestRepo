51e8bd5baa33e356642e7c1e3fcc676b9bf74b46
huh445 <127354663+huh445@users.noreply.github.com>
2025-01-03 16:52:07 -0800

    improve optimisation for my vscode because old mac

--- semester 1/pracsacs/pracsacj39/pracsac2024/login/get_welcome.py
+++
@@ -1,7 +0,0 @@
-class GetWelcome:
-    def __init__(self):
-        pass
-    def get_text(self):
-        with open("pracsac2024/login/welcome.txt", mode="r") as f:
-            reader = f.read()
-        return reader
